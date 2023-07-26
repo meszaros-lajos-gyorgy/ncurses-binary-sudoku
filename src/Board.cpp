8c91f2588bfa317e18e701a450f1a943dd696554
Lajos Meszaros <m_lajos@hotmail.com>
2023-07-26 23:11:11 +0200

    feat(Board): add some validation to when reading and writing tiles at specific coordinates

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,4 +1,3 @@
-#include <stdexcept>
 #include "Board.hpp"
 
 Board::Board(uint8_t width, uint8_t height) {
@@ -13,42 +12,57 @@ void Board::populate() {
     this->tiles->at(i) = new Tile(UNDEFINED);
   }
 
-  this->setTileAt(1, 0, new Tile(ZERO));
-  this->setTileAt(2, 0, new Tile(ZERO));
-  this->setTileAt(5, 0, new Tile(ONE));
+  // a simple 8x8 puzzle
+  if (this->width == 8 && this->height == 8) {
+    this->setTileAt(1, 0, new Tile(ZERO));
+    this->setTileAt(2, 0, new Tile(ZERO));
+    this->setTileAt(5, 0, new Tile(ONE));
 
-  this->setTileAt(0, 1, new Tile(ONE));
-  this->setTileAt(1, 1, new Tile(ZERO));
-  this->setTileAt(3, 1, new Tile(ONE));
-  this->setTileAt(5, 1, new Tile(ONE));
+    this->setTileAt(0, 1, new Tile(ONE));
+    this->setTileAt(1, 1, new Tile(ZERO));
+    this->setTileAt(3, 1, new Tile(ONE));
+    this->setTileAt(5, 1, new Tile(ONE));
 
-  this->setTileAt(7, 2, new Tile(ZERO));
+    this->setTileAt(7, 2, new Tile(ZERO));
 
-  this->setTileAt(1, 3, new Tile(ONE));
-  this->setTileAt(6, 3, new Tile(ONE));
-  this->setTileAt(7, 3, new Tile(ZERO));
+    this->setTileAt(1, 3, new Tile(ONE));
+    this->setTileAt(6, 3, new Tile(ONE));
+    this->setTileAt(7, 3, new Tile(ZERO));
 
-  this->setTileAt(0, 4, new Tile(ZERO));
-  this->setTileAt(4, 4, new Tile(ONE));
+    this->setTileAt(0, 4, new Tile(ZERO));
+    this->setTileAt(4, 4, new Tile(ONE));
 
-  this->setTileAt(3, 5, new Tile(ZERO));
-  this->setTileAt(7, 5, new Tile(ONE));
+    this->setTileAt(3, 5, new Tile(ZERO));
+    this->setTileAt(7, 5, new Tile(ONE));
 
-  this->setTileAt(1, 6, new Tile(ONE));
-  this->setTileAt(4, 6, new Tile(ONE));
+    this->setTileAt(1, 6, new Tile(ONE));
+    this->setTileAt(4, 6, new Tile(ONE));
 
-  this->setTileAt(7, 7, new Tile(ONE));
-}
-
-Tile * Board::getTileAt(uint8_t x, uint8_t y) {
-  try {
-    return this->tiles->at(y * this->height + x);
-  } catch(const std::out_of_range & err) {
-    return nullptr;
+    this->setTileAt(7, 7, new Tile(ONE));
   }
 }
 
+Tile * Board::getTileAt(uint8_t x, uint8_t y) {
+  if (x < 0 || x >= this->width) {
+    return nullptr;
+  }
+
+  if (y < 0 || y >= this->height) {
+    return nullptr;
+  }
+
+  return this->tiles->at(y * this->height + x);
+}
+
 void Board::setTileAt(uint8_t x, uint8_t y, Tile * value) {
+  if (x < 0 || x >= this->width) {
+    return;
+  }
+
+  if (y < 0 || y >= this->height) {
+    return;
+  }
+
   this->tiles->at(y * this->height + x) = value;
 }
 
